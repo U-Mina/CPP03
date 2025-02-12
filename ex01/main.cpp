12ed2fa84dab175ead93b75d7c73dc866d24b872
Erya Wu <ewu@2-G-2.42heilbronn.de>
2025-02-12 22:59:09 +0100

    wrote ex01 as child class of ex00, inherit attribute and functions (takedamege(), repair()), and override attack()

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,10 +6,11 @@
 /*   By: ewu <ewu@student.42heilbronn.de>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/12 19:38:10 by ewu               #+#    #+#             */
-/*   Updated: 2025/02/12 21:34:12 by ewu              ###   ########.fr       */
+/*   Updated: 2025/02/12 22:58:18 by ewu              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "ScavTrap.hpp"
 #include "ClapTrap.hpp"
 
 int main()
@@ -17,13 +18,21 @@ int main()
 	//test default
 	ClapTrap deFault;
 	deFault.attack("defaultEnemy");
+	ScavTrap defaultS;
+	defaultS.attack("default_inScavTrap");
 	
-	ClapTrap tester1("Mikey");
-	tester1.attack("Mario");
+	// ClapTrap tester1("Mikey");
+	// tester1.attack("Mario");
+	ScavTrap tester2("Princess Happy");
+	tester2.attack("cherry");
 	
-	tester1.takeDamage(5);
-	tester1.beRepaired(3);
-	tester1.attack("someone");
+	// tester1.takeDamage(5);
+	// tester1.beRepaired(3);
+	//tester1.attack("someone");
+	tester2.takeDamage(59);
+	tester2.beRepaired(39);
+	tester2.attack("NOTGOOD");
+	tester2.guardGate();
 	return 0;
 }
 
