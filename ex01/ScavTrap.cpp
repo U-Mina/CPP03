58e29d3767123faa4e56f84d8cb2b5e828895470
Erya Wu <ewu@2-G-2.42heilbronn.de>
2025-02-12 22:51:47 +0100

    grammar in inheritance and override attack()

---
+++ ex01/ScavTrap.cpp
@@ -0,0 +1,73 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ScavTrap.cpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ewu <ewu@student.42heilbronn.de>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/12 22:06:54 by ewu               #+#    #+#             */
+/*   Updated: 2025/02/12 22:51:22 by ewu              ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ScavTrap.hpp"
+#include <iostream>
+
+ScavTrap::ScavTrap() : ClapTrap("default")
+{
+	HitPoints = 100;
+	EnergyPoints = 50;
+	AttackDamage = 20;
+	std::cout << "Default ScavTrap constrcutor is called!" << "\n";
+}
+
+ScavTrap::ScavTrap(const std::string& name) : ClapTrap(name)
+{
+	HitPoints = 100;
+	EnergyPoints = 50;
+	AttackDamage = 20;
+}
+ScavTrap::ScavTrap(const ScavTrap& other)
+{
+	*this = other;
+}
+ScavTrap& ScavTrap::operator=(const ScavTrap& other)
+{
+	if (this != &other)
+	{
+		name = other.name;
+		HitPoints = other.HitPoints;
+		EnergyPoints = other.EnergyPoints;
+		AttackDamage = other.AttackDamage;
+		//or a simpler syntax:
+		// ClapTrap::operator=(other); ==> call base class '=' operator!
+	}
+	return *this;
+}
+
+ScavTrap::~ScavTrap()
+{
+	std::cout << "ScavTrap Destructor is called!" << "\n";
+}
+
+void ScavTrap::attack(const std::string& target)
+{
+	if (this->EnergyPoints <= 0)
+	{
+		std::cout << "ScavTrap: No Energy left!" << "\n";
+		return ;
+	}
+	if (this->HitPoints <= 0 )
+	{
+		std::cout << "ScavTrap: No HP!" << target << " dead!" << "\n";
+		return ;	
+	}
+	this->EnergyPoints--;
+	std::cout << "This is attack from ScavTrap: " << "\n";
+	std::cout << "ScavTrap " << name << " attacks " << target
+				<< ", causing " << AttackDamage << " points of damage!" << "\n";
+}
+void ScavTrap::guardGate()
+{
+	std::cout << "ScavTrap: " << name << " is now in Gate Keeper mode." << "\n";
+}
--- ex01/ScavTrap.hpp
+++ ex01/ScavTrap.hpp
@@ -1,38 +1,32 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ClapTrap.hpp                                       :+:      :+:    :+:   */
+/*   ScavTrap.hpp                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ewu <ewu@student.42heilbronn.de>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/02/12 15:21:04 by ewu               #+#    #+#             */
-/*   Updated: 2025/02/12 22:18:06 by ewu              ###   ########.fr       */
+/*   Created: 2025/02/12 22:07:05 by ewu               #+#    #+#             */
+/*   Updated: 2025/02/12 22:46:39 by ewu              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef CLAPTRAP_HPP
-#define CLAPTRAP_HPP
+#ifndef SCAVTRAP_HPP
+#define SCAVTRAP_HPP
 
-#include <string>
+#include "ClapTrap.hpp"
 
-class ClapTrap
+class ScavTrap : public ClapTrap
 {
-//private:
-protected://for ScavTrap class to access ClapTrap attribute directly
-	std::string name;
-	int HitPoints;
-	int EnergyPoints;
-	int AttackDamage;
 public:
-	ClapTrap();
-	ClapTrap(const std::string& name);//check
-	ClapTrap(const ClapTrap& other);
-	ClapTrap& operator=(const ClapTrap& other);
-	~ClapTrap();
+	ScavTrap();
+	ScavTrap(const std::string& name);
+	ScavTrap(const ScavTrap& other);
+	ScavTrap& operator=(const ScavTrap& other);
+	~ScavTrap();
 	
+	//override attack() of ClapTrap
 	void attack(const std::string& target);
-	void takeDamage(unsigned int amount);
-	void beRepaired(unsigned int amount);
+	void guardGate();
 };
 
 #endif
