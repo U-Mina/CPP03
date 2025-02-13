c490236618fc7be587ea797bed4900ca56dfafc3
Erya Wu <ewu@2-G-2.42heilbronn.de>
2025-02-13 14:32:41 +0100

    wrote customized tester

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,33 +6,45 @@
 /*   By: ewu <ewu@student.42heilbronn.de>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/12 19:38:10 by ewu               #+#    #+#             */
-/*   Updated: 2025/02/12 22:58:18 by ewu              ###   ########.fr       */
+/*   Updated: 2025/02/13 14:31:17 by ewu              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ScavTrap.hpp"
 #include "ClapTrap.hpp"
+#include "FragTrap.hpp"
 
 int main()
 {
 	//test default
-	ClapTrap deFault;
-	deFault.attack("defaultEnemy");
-	ScavTrap defaultS;
-	defaultS.attack("default_inScavTrap");
+	ClapTrap claptrap;
+	claptrap.attack("clap");
+	ScavTrap scavtrap;
+	scavtrap.attack("scav");
+	FragTrap fragtrap;
+	fragtrap.attack("frag");
 	
-	// ClapTrap tester1("Mikey");
-	// tester1.attack("Mario");
+	ClapTrap tester1("Mikey");
+	tester1.attack("Mario");
 	ScavTrap tester2("Princess Happy");
 	tester2.attack("cherry");
+	FragTrap tester3("Lemon");
+	tester3.attack("Tree");
 	
-	// tester1.takeDamage(5);
-	// tester1.beRepaired(3);
-	//tester1.attack("someone");
+	tester1.takeDamage(5);
+	tester1.beRepaired(3);
+	tester1.attack("1st bad guy");
+	
 	tester2.takeDamage(59);
 	tester2.beRepaired(39);
-	tester2.attack("NOTGOOD");
+	tester2.attack("2nd bad guy");
 	tester2.guardGate();
+	
+	tester3.takeDamage(90);
+	tester3.beRepaired(77);
+	tester3.attack("3rd bad guy");
+	tester3.highFivesGuys();
+	
 	return 0;
 }
 
